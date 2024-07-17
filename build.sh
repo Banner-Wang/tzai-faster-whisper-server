55c6a7e7b9a8feb850beafcda37003a2211ad5a6
BannerWang <banner.wang@uptech.ai>
2024-07-17 16:25:27 +0800

    build: pin FastAPI, Uvicorn, and Pydantic versions for compatibility
    Update the specified Python package versions in the build script to ensure
    compatibility with the current codebase. This includes FastAPI 0.111.0,
    Uvicorn0.30.1, and Pydantic 2.8.2.

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ apt-get update && apt-get install -y ffmpeg jq file \
 # 安装 Python 依赖包，先升级 pip，然后安装所需的 Python 包
 /opt/python3/bin/pip3 install --no-cache-dir --upgrade pip
 
-/opt/python3/bin/pip3 install loguru fastapi uvicorn pydantic pydantic_settings ffmpeg-python faster_whisper openai-whisper
+/opt/python3/bin/pip3 install loguru fastapi==0.111.0 uvicorn==0.30.1 pydantic==2.8.2 pydantic_settings==2.3.4 ffmpeg-python faster_whisper openai-whisper
